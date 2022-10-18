598f47cbf64cd4bc766996a9e2f2a009cfb25167
BHARATHI B <104187988+bharathisan@users.noreply.github.com>
2022-10-18 15:43:28 +0530

    Add files via upload

---
+++ gitHub.java
@@ -0,0 +1,40 @@
+package github;
+
+public class gitHub {
+
+	public static void main(String[] args) {
+		
+				//declaration of variables
+			int intval;
+			float fval;
+			double dval;
+			char cval;
+			short sval;
+			byte bval;
+			long lval;
+			boolean boolval;
+			
+			//Assign the values
+			intval=988;
+			
+			System.out.println("Integer value=" +intval);
+			fval=78.9f;
+			System.out.println("Float value=" +fval);
+			
+			dval=98.9;
+			cval='B';
+			lval=8976;
+			boolval=true;
+			
+			System.out.println("Double value=" +dval);
+			System.out.println("Char value=" +cval);
+			System.out.println("Long value=" +lval);
+			System.out.println("Boolean value=" +boolval);
+
+			}
+
+		
+
+	}
+
+
